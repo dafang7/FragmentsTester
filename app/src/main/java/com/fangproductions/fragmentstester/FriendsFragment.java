75408148259611c2fc5e933423f34684d58a0335
Derek F <dafang7@gmail.com>
2015-02-24 16:03:53 -0600

    Using "On Resume" I was able to populate the list and add a TextView in the test file.

--- app/src/main/java/com/fangproductions/fragmentstester/FriendsFragment.java
+++ app/src/main/java/com/fangproductions/fragmentstester/FriendsFragment.java
@@ -5,16 +5,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by derek.fang on 2/9/2015.
  */
 public class FriendsFragment extends android.support.v4.app.ListFragment {
 
+    private TextView mTextView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
         return rootView;
+    }
+
+    //ON RESUME SAVES ME
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mTextView = (TextView) getActivity().findViewById(R.id.centerTextView);
+        mTextView.setText("FINALLY");
     }
 }
--- app/src/main/java/com/fangproductions/fragmentstester/InboxFragment.java
+++ app/src/main/java/com/fangproductions/fragmentstester/InboxFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 /**
@@ -20,5 +21,19 @@ public class InboxFragment extends android.support.v4.app.ListFragment {
         View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
 
         return rootView;
+    }
+
+    //Apparently updating views and stuff needs to be done in On Resume....
+    @Override
+    public void onResume() {
+        super.onResume();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getListView().getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.android_names));
+        setListAdapter(adapter);
+
+        mCenterTextView = (TextView) getActivity().findViewById(R.id.centeredText);
+        mCenterTextView.setText("OVERLAY");
     }
 }
