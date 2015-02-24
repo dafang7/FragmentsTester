5b91a8c3f492bb5ac4aea1bf1f8b8e41c8be32ce
Derek F <dafang7@gmail.com>
2015-02-24 14:20:29 -0600

    Added all the classes and layouts

---
+++ app/src/main/java/com/fangproductions/fragmentstester/FriendsFragment.java
@@ -0,0 +1,20 @@
+package com.fangproductions.fragmentstester;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by derek.fang on 2/9/2015.
+ */
+public class FriendsFragment extends android.support.v4.app.ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/fangproductions/fragmentstester/InboxFragment.java
@@ -0,0 +1,20 @@
+package com.fangproductions.fragmentstester;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by derek.fang on 2/9/2015.
+ */
+public class InboxFragment extends android.support.v4.app.ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+        return rootView;
+    }
+}
